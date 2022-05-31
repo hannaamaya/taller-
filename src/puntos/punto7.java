e2e49997f8d3b7ef1fba942e3713f56b3516b3e4
hannaamaya <101846308+hannaamaya@users.noreply.github.com>
2022-05-30 19:36:58 -0500

    punto7
    se agrego el =

--- src/puntos/punto7.java
+++ src/puntos/punto7.java
@@ -13,21 +13,21 @@ import javax.swing.JOptionPane;
 public class punto7 {
     public static void main(String[] args) {
         
-        int A=0;
-        int B=0;
-        int C=0;
+        int A;
+        int B;
+        int C;
         
          A= Integer.parseInt(JOptionPane.showInputDialog(null,"Digite el valor de A: "));
          B= Integer.parseInt(JOptionPane.showInputDialog(null,"Digite el valor de B: "));
          C= Integer.parseInt(JOptionPane.showInputDialog(null,"Digite el valor de C: "));
          
-         if (A>B&&B>C){
-             JOptionPane.showMessageDialog(null,"El nùmero menor es A");
-             }if(B>A&&B>C){
-             JOptionPane.showMessageDialog(null,"El nùmero menor es B");
-             }if(C>A&&C>B){
-             JOptionPane.showMessageDialog(null,"El nùmero menor es C");
-             }  
+         if (A<=B&&B<=C){
+             JOptionPane.showMessageDialog(null,"El nùmero menor es: "+A);
+             }else if(B<=A&&B<=C){
+             JOptionPane.showMessageDialog(null,"El nùmero menor es: "+B);
+             }else if(C<=A&&C<=B){
+             JOptionPane.showMessageDialog(null,"El nùmero menor es: "+C);
+             }
     }
     
 }
