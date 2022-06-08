93ff54b597b010aef6890365eddf72da3c0996ef
hannaamaya <101846308+hannaamaya@users.noreply.github.com>
2022-06-08 17:21:26 -0500

    correccion
    correcciones

--- src/puntos/punto15.java
+++ src/puntos/punto15.java
@@ -12,25 +12,23 @@ import javax.swing.JOptionPane;
  */
 public class punto15 {
     public static void main(String[] args) {
-        
+        // declaracion de variables, numeros decimales
         float num,numerosaSumar,suma;
         float media;
-        
-        
-        
+        //inicializacion variable
         suma=0; 
+        //se realizo el for para realizar 10 veces la suma
         for (int i = 0; i < 10; i++) {
             num=Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el numero negativo : "));
             if(num<0){
-            numerosaSumar=num;
-            suma=suma+numerosaSumar;
+                numerosaSumar=num;
+                suma=suma+numerosaSumar;
             }else{
-            JOptionPane.showMessageDialog(null, "el numero no es negativo");
+                JOptionPane.showMessageDialog(null, "el numero no es negativo");
             }  
         }
-          media=suma/10;
-          JOptionPane.showMessageDialog(null, "la media es: "+media);
-          
+        media=suma/10;
+        JOptionPane.showMessageDialog(null, "la media es: "+media);
     }   
     
 }
